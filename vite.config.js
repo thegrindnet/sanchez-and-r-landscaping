34333c3cea258208b366644bb625a1dd17c72370
David Bonilla <thegrindnet@gmail.com>
2026-09-07 00:11:14 -0600

    Change base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/sanchez-and-r-landscaping",
   server: { port: 3000 },
 });
