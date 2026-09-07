9b7c4d79cd7c659f4cf7d7a284510db09d0c8bb2
David Bonilla <thegrindnet@gmail.com>
2026-09-07 00:52:18 -0600

    Add last / in base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/sanchez-and-r-landscaping",
+  base: "/sanchez-and-r-landscaping/",
   server: { port: 3000 },
 });
